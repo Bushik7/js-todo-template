a62c35054d4024f72079162903c56407d7678c43
Данил <bushevdan@rambler.ru>
2022-06-04 16:44:19 +0300

    create project

---
+++ index.js
@@ -0,0 +1,14 @@
+const tasks = [
+    {"id":"1","text":"synthesize","completed":true},
+    {"id":"2","text":"override","completed":false},
+    {"id":"3","text":"index","completed":true},
+    {"id":"4","text":"compress","completed":false},
+    {"id":"5","text":"compress","completed":false},
+    {"id":"6","text":"override","completed":true},
+    {"id":"7","text":"generate","completed":true}
+];
+
+console.log(tasks); 
+
+
+
